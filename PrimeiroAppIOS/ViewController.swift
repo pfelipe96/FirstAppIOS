7ecf86904194fa296bb2ed0f0ef8234083a24b1c
Paulo <p.felipe@msn.com>
2018-09-17 08:25:12 -0300

    Initial Commit

---
+++ PrimeiroAppIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimeiroAppIOS
+//
+//  Created by Paulo Felipe Oliveira Souza on 17/09/2018.
+//  Copyright © 2018 Paulo Felipe Oliveira Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
